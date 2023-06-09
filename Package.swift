aa8895f5c90d33ea72360f4f45a50fa0c847a5df
Kevin Wooten <kevin@outfoxx.io>
2023-06-09 15:48:31 -0700

    Update to PotentCodables 3.0.3 to include support for random data resilience
    This is required to ensure when attempting to decode structures decrypted with an incorrect key an error is thrown.
    
    This is because PKCS#8 requires AES-CBC which is not authenticated and cannot itself detect invalid passwords.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
       targets: ["Shield", "ShieldSecurity", "ShieldCrypto", "ShieldOID", "ShieldPKCS", "ShieldX509", "ShieldX500"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/outfoxx/PotentCodables.git", from: "3.0.0"),
+    .package(url: "https://github.com/outfoxx/PotentCodables.git", from: "3.0.3"),
     .package(url: "https://github.com/sharplet/Regex.git", from: "2.1.0"),
     .package(name: "Algorithms", url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
   ],
